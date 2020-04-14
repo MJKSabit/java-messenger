effef82303496de24ff3a7ce9d5d14c991e40ae7
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-04-14 09:21:32 +0600

    SceneBuilder Design

--- messenger-client-gui/src/sample/Controller.java
+++ messenger-client-gui/src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    TextField txt_username;
+    @FXML
+    PasswordField txt_password;
+
+
+    public void signUp() {
+        System.out.println(txt_username.getCharacters().toString());
+        System.out.println(txt_password.getCharacters().toString());
+    }
 }
